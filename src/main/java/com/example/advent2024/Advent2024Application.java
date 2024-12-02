00383e74017259a1916b0d9e88dbc70a918a2409
Marcin Wasiluk <marcin@wasiluk.com>
2024-12-02 17:58:37 +0000

    init

---
+++ src/main/java/com/example/advent2024/Advent2024Application.java
@@ -0,0 +1,13 @@
+package com.example.advent2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Advent2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Advent2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advent2024/Advent2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advent2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Advent2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
