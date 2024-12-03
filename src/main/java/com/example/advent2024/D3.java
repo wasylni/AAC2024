fc5bc0f5a0409461d260f4cb18e6bbabb19fbe14
Marcin Wasiluk <marcin@wasiluk.com>
2024-12-03 16:35:46 +0000

    get D3 input

--- src/main/java/com/example/advent2024/D3.java
+++ src/main/java/com/example/advent2024/D3.java
@@ -1,6 +1,8 @@
 package com.example.advent2024;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,6 +27,47 @@ public class D3 {
         return total;
     }
 
+    static Integer calculateMulls2(String inputData) {
+        int total = 0;
+
+        String regex = "\\w*do\\(\\)|\\w*don't\\(\\)|mul\\(\\d+,\\d+\\)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(inputData);
+
+        List<String> matches = new ArrayList<>();
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        boolean include = false;
+        for (int i = 0; i < matches.size(); i++) {
+            String currentItem = matches.get(i);
+            String previousItem = (i > 0) ? matches.get(i - 1) : null; // Check for previous item
+
+            System.out.println("Current item: " + currentItem);
+            if (previousItem != null) {
+                if (previousItem.contains("don't()")) {
+                    include = false;
+                }
+                if (previousItem.contains("do()")) {
+                    include = true;
+                }
+                System.out.println("Previous item: " + previousItem);
+                if (include) {
+                    total = total + calculate(currentItem);
+                }
+            } else {
+                if (!currentItem.contains("don't()")) {
+                    total = total + calculate(currentItem);
+                }
+                include = true;
+                System.out.println("No previous item (first element).");
+            }
+        }
+
+        System.out.println("Extracted matches: " + matches);
+        return total;
+    }
+
     private static int calculate(String text) {
         String regex = "mul\\((\\d+),(\\d+)\\)";
         Pattern pattern = Pattern.compile(regex);
@@ -37,8 +80,8 @@ public class D3 {
 
             System.out.println("First number: " + firstNumber);
             System.out.println("Second number: " + secondNumber);
-            System.out.println("Sum: " + firstNumber + "*" +secondNumber +"="+(firstNumber*secondNumber));
-            return (firstNumber*secondNumber);
+            System.out.println("Sum: " + firstNumber + "*" + secondNumber + "=" + (firstNumber * secondNumber));
+            return (firstNumber * secondNumber);
         } else {
             System.out.println("No match found!");
         }
