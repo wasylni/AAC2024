7a066bd59a76586cf95f08f9e53917c0469c74a4
Marcin Wasiluk <marcin@wasiluk.com>
2024-12-04 18:38:29 +0000

    get D1 additional

--- src/main/java/com/example/advent2024/D1.java
+++ src/main/java/com/example/advent2024/D1.java
@@ -4,9 +4,10 @@ package com.example.advent2024;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class D1 {
-    Integer calculate(String input){
+    Integer calculate(String input) {
         // Lists to store the separated numbers
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
@@ -43,6 +44,50 @@ public class D1 {
 
         System.out.println("**** Result for D1 is " + result);
         return result;
+    }
+
+    Integer calculateSimilarity(String input) {
+        final int[] totalSimilarityScore = {0};
+        // Lists to store the separated numbers
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        // Split the input into lines
+        String[] lines = input.split("\\n");
+
+        for (String line : lines) {
+            // Split each line into two numbers
+            String[] parts = line.trim().split("\\s+");
+            list1.add(Integer.parseInt(parts[0])); // First number
+            list2.add(Integer.parseInt(parts[1])); // Second number
+        }
+        list1.forEach(numberL1 -> {
+            int similarityNo = getReocurrenceCount(numberL1, list2);
+            totalSimilarityScore[0] = (int) (totalSimilarityScore[0] + (numberL1 * similarityNo));
+        });
+
+        return totalSimilarityScore[0];
+    }
+
+    private static int getReocurrenceCount(Integer numberL1, List<Integer> list2) {
+        AtomicInteger found = new AtomicInteger();
+        list2.forEach(numberL2 ->
+                {
+                    if (comparenumbers(numberL1, numberL2)) {
+                        found.set(found.get() + 1);
+                    }
+                }
+
+        );
+        return found.get();
+    }
+
+    private static boolean comparenumbers(int numberL1, int number2) {
+        if (number2 == numberL1) {
+            System.out.println(numberL1);
+            return true;
+        }
+
+        return false;
     }
 
 }
